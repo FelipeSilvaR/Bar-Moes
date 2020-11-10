ca792d8cdb07b3a4cb129a260e4c32fbc7c05d38
felipe silva <felipe.silvar85@gmail.com>
2020-11-09 22:48:23 -0300

    Generar la interfaz de productos
    Generar las url de productos
    Agregar como dependencia humanize para formatear precio
    Agregar condiciones if en las herramientas de templating
    Implementar font awesome
    Generar la condicion de pagina activa mediante request.path

--- web_moes/core/urls.py
+++ web_moes/core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('contacto/', views.contacto, name="contacto")
+    path('contacto/', views.contacto, name="contacto"),
+    
 ]
